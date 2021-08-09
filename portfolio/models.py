65c4d51a9b0fd3e5accc92f07e2b993c0ac37090
Devin Baack <66151670+DBaack11@users.noreply.github.com>
2021-08-09 01:24:38 -0500

    About Page and Styling Update

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    video = models.FileField(upload_to='portfolio/videos')
+    video = models.FileField(upload_to='portfolio/videos', blank=True)
     github = models.URLField(blank=True)
     tools = models.CharField(max_length=200)
     writeUp = models.TextField()
--- portfolio/views.py
+++ portfolio/views.py
@@ -7,5 +7,6 @@ def home(request):
     projects = Project.objects.all()
     return render(request, 'home.html', {'projects': projects})
 
-
+def about(request):
+    return render(request, 'about.html')
 
