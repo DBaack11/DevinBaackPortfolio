52464abe260d37525561f6a811a6e5aa100cf23f
Devin Baack <66151670+DBaack11@users.noreply.github.com>
2021-08-19 15:38:28 -0500

    Styling/Layout Update

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     video = models.URLField(blank=True)
+    image = models.URLField(blank=True)
     github = models.URLField(blank=True)
     tools = models.CharField(max_length=200)
     writeUp = models.TextField()
